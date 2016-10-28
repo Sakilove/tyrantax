85533b238bd3800c335c6cbdf75a058265389722
雍世康 <tyrantax43@gmail.com>
2016-10-29 05:33:15 +0800

    setup

--- js/common.js
+++ js/common.js
@@ -59,7 +59,7 @@ function processPageView(rows) {
 
 LazyLoad.css('/tyrantax/css/font.css');
 
-LazyLoad.js('site.url/js/jquery-1.11.1.min.js', function () {
+LazyLoad.js('/tyrantax/js/jquery-1.11.1.min.js', function () {
     hideRecentSectionWhenNoPost();
 
     $('h1').each(function() {
@@ -74,12 +74,12 @@ LazyLoad.js('site.url/js/jquery-1.11.1.min.js', function () {
     });
 
     if(/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) === false) {
-        LazyLoad.js('{{ site.url }}/js/jquery.timeago.min.js', function () {
+        LazyLoad.js('/tyrantax/js/jquery.timeago.min.js', function () {
             $('.timeago').timeago().show();
         });
     }
 
-    LazyLoad.js('{{site.url}}/js/unviel.min.js', function () {
+    LazyLoad.js('/tyrantax/js/unviel.min.js', function () {
         $("img").unveil();
 
         // google pageview
@@ -124,8 +124,8 @@ LazyLoad.js('site.url/js/jquery-1.11.1.min.js', function () {
 
 // emoji
 setTimeout(function() {
-    LazyLoad.css('{{ site.url }}/css/emojify.min.css', function () {
-        LazyLoad.js('{{ site.url }}/js/emojify.min.js', function () {
+    LazyLoad.css('/tyrantax/css/emojify.min.css', function () {
+        LazyLoad.js('/tyrantax/js/emojify.min.js', function () {
             emojify.setConfig({
                 emoticons_enabled: true,
                 people_enabled: true,
